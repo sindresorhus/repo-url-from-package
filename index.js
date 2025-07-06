d2c8a9a5dd24d3489462a6c0ead6a197ab2762b6
Sindre Sorhus <sindresorhus@gmail.com>
2025-07-07 00:42:26 +0200

    Require Node.js 20
    Closes #3

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import githubUrlFromGit from 'github-url-from-git';
-import isUrl from 'is-url-superb';
 
 // Look for GitHub shorthand inputs such as "user/repo#branch".
 const githubShorthandRegex = /^(\w(?:-?\w){0,38}\/[\w.-]{1,100})(#\S*)?$/;
@@ -28,7 +27,7 @@ export default function repoUrlFromPackage(packageJson) {
 	if (!url) {
 		url = repoUrl;
 
-		if (isUrl(url) && /^https?:\/\//.test(url)) {
+		if (/^https?:\/\//.test(url) && URL.canParse(url)) {
 			warnings.push(`The \`repository\` field in package.json should point to a Git repo and not a website. Please open an issue or pull request on \`${packageJson.name}\`.`);
 		} else {
 			warnings.push(`The \`repository\` field in package.json is invalid. Please open an issue or pull request on \`${packageJson.name}\`.`);
